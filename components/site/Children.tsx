627d97e1d968ee98b1ed93c39726e44ca6d23519
mcmanyika <mcmanyika.com>
2023-04-23 18:03:58 -0500

    children ministries

--- components/site/Children.tsx
+++ components/site/Children.tsx
@@ -3,12 +3,12 @@ import Image from "next/image"
 function Children() {
   return (
     <div>
-        <Image src="/a1.jpeg" 
+        <Image src="/s3.jpeg" 
         alt="Picture of the author"
         width={450}
         height={80} />
         <div>
-        <p className="text-xl font-semibold mt-5 mb-5">Spiritual Growth </p>
+        <p className="text-xl font-semibold mt-5 mb-5">Children{`'`}s Ministry </p>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
 
