e9152805b10aa6cab6133c280249b3470eb8e823
mcmanyika <mcmanyika.com>
2023-04-23 17:09:16 -0500

    changed article

--- components/site/Layout.tsx
+++ components/site/Layout.tsx
@@ -12,7 +12,7 @@ function Layout({...props}) {
   return (
     <>
     <div>
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
             <Menu />
             {props.children}
             <Footer />
