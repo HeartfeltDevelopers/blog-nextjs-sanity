778abde01f6ed317720d8fcff01f92c386a37eb5
mcmanyika <mcmanyika.com>
2023-04-23 22:30:22 -0500

    footer logo

--- components/site/Footer.tsx
+++ components/site/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import About from "./About"
 import Conferences from "./Conferences"
 import Info from "./Info"
@@ -20,6 +21,14 @@ function Footer() {
         </div>
         <div className='flex-1'>
            <Info />
+        </div>
+      </div>
+      <div className="grid grid-flow-row auto-rows-max  mx-auto md:w-4/6 pt-10">
+        <div className="text-xs opacity-50 flex items-center ">
+          <Image src={`/logo.png`} width={40} height={40} alt=""/>
+          <div className="p-3">
+              Copyright © 2023 Heartfelt International Ministries
+            </div>
         </div>
       </div>
     </div>
